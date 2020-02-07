eb51bb598be4bd128e2f46b20365941a4026e9d7
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-02-07 02:47:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,8 +9,8 @@ end
 
 def print_first_directors_movie_titles
    counter = 0
-  while counter < pretty_print_nds[0][:movies].count do
-    puts pretty_print_nds[0][:movies][counter][:title]
+  while counter < directors_database[0][:movies].count do
+    puts directors_database[0][:movies][counter][:title]
     counter += 1
   end
 end
